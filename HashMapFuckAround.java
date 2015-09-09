b00aac73cc0f161b36bd8d2459092ab6843cb7a6
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-08 22:42:24 -0400

    Things of great import were addeed

--- HashMapFuckAround.java
+++ HashMapFuckAround.java
@@ -12,10 +12,11 @@ public class HashMapFuckAround {
 		// populate the price list
 		Map<String, String> barPrice= new HashMap<String,String>(){{
 			//TODO add the rest of the bars
-			put("Oh Henry      ", "0.65");
-			put("Coffee Crisp  ", "0.80");
-			put("Aero          ", "0.60");
-			put("Crunchie      ", "0.75");
+			put("Oh Henry", "0.65");
+			put("Coffee Crisp", "0.80");
+			put("Aero", "0.60");
+			put("Smarties", "0.70");
+			put("Crunchie", "0.75");
 	
 		}};
 	// Make the hash to store the amounts
@@ -23,21 +24,20 @@ public class HashMapFuckAround {
 	// Loop over the entries 
 		for (Map.Entry<String, String>entry : barPrice.entrySet()) {
 			// Take the names from barPrice, get amount from Console
-			System.out.println("How many boxes of " + entry.getKey());
-			barNum.put(entry.getKey(), new Scanner(System.in).nextLine());
-			System.out.println(barNum.get(entry.getKey()));
-			
+			System.out.print("How many boxes of " + entry.getKey() + " ");
+			barNum.put(entry.getKey(), new Scanner(System.in).next());
+			}
 		
-		
+	//loop again
+		float subTotal = 0;
+		for(Map.Entry<String, String>entry : barPrice.entrySet()) {
+			String barName = entry.getKey();
+			String barAmt = barNum.get(barName);
+			float barTotal = (Float.parseFloat(barPrice.get(barName)) * (Float.parseFloat(barAmt)) * 48);
+			System.out.print(barAmt + " boxes of " + barName + " ($" + barPrice.get(barName) + " x 48) = $(" );
+			System.out.printf("%.2f)\n",barTotal);
+			subTotal += barTotal;
+			System.out.print(subTotal);
 		}
-		
-		//loop again
-	
-	//	for(Map.Entry<String, String>entry : barPrice.entrySet()) {
-	//		System.out.println("How many boxes of " + entry.getKey() + " would you like (48 bars per box) ? ");
-		//	String barName = entry.getKey();
-		//	String barVal = entry.getValue();1
-		//	System.out.println(entry);
-		}
-	//	System.out.println(BarHash.toString());
-		}
+	}
+}
