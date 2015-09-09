a5c6e535b319e38bdefa11039066dabe72abaed6
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-08 21:42:08 -0400

    Add second HashMap

--- HashMapFuckAround.java
+++ HashMapFuckAround.java
@@ -10,22 +10,30 @@ public class HashMapFuckAround {
 	public static void main(String[] args) {
 		// populate the price list
 		Map<String, String> barPrice= new HashMap<String,String>(){{
+			//TODO add the rest of the bars
 			put("Oh Henry      ", "0.65");
 			put("Coffee Crisp  ", "0.80");
 			put("Aero          ", "0.60");
 			put("Crunchie      ", "0.75");
 	
 		}};
+		Map<String, String> barNum = new HashMap<String, String>();
 	// Loop over the entries 
 		for (Map.Entry<String, String>entry : barPrice.entrySet()) {
-			System.out.println(entry.getKey() +" " + entry.getValue());
+			// Take the names from barPrice, get amount from Consol
+			barNum.put(entry.getKey(), "foo");
+			
+			System.out.println(barNum.keySet());
+		//	System.out.println(entry.getKey() +" " + entry.getValue());
 		}
-		for(Map.Entry<String, String>entry : barPrice.entrySet()) {
-			System.out.println("How many boxes of " + entry.getKey() + " would you like (48 bars per box) ? ");
+		
+		//loop again
+	
+	//	for(Map.Entry<String, String>entry : barPrice.entrySet()) {
+	//		System.out.println("How many boxes of " + entry.getKey() + " would you like (48 bars per box) ? ");
 		//	String barName = entry.getKey();
-		//	String barVal = entry.getValue();
+		//	String barVal = entry.getValue();1
 		//	System.out.println(entry);
 		}
 	//	System.out.println(BarHash.toString());
 		}
-}
