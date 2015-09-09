994fae69b11ce358f5f588f6ba0f7f43177791e3
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-08 21:36:01 -0400

    Gave up on nested HashMaps, switching to seperate hashes for data

--- HashMapFuckAround.java
+++ HashMapFuckAround.java
@@ -9,19 +9,18 @@ public class HashMapFuckAround {
 	 */
 	public static void main(String[] args) {
 		// populate the price list
-		Map<String, String> BarHash = new HashMap<String,String>(){{
+		Map<String, String> barPrice= new HashMap<String,String>(){{
 			put("Oh Henry      ", "0.65");
 			put("Coffee Crisp  ", "0.80");
 			put("Aero          ", "0.60");
-			put("Smarties      ", "0.70");
 			put("Crunchie      ", "0.75");
 	
 		}};
 	// Loop over the entries 
-		for (Map.Entry<String, String>entry : BarHash.entrySet()) {
+		for (Map.Entry<String, String>entry : barPrice.entrySet()) {
 			System.out.println(entry.getKey() +" " + entry.getValue());
 		}
-		for(Map.Entry<String, String>entry : BarHash.entrySet()) {
+		for(Map.Entry<String, String>entry : barPrice.entrySet()) {
 			System.out.println("How many boxes of " + entry.getKey() + " would you like (48 bars per box) ? ");
 		//	String barName = entry.getKey();
 		//	String barVal = entry.getValue();
