0af45ecef507573c7ba3b54b8d6b842cc0c303a3
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-08 23:26:46 -0400

    All required code written, almost ready to hand in

--- HashMapFuckAround.java
+++ HashMapFuckAround.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
+ // TODO take fuck out of the class name
 public class HashMapFuckAround {
 
 	/**
@@ -11,7 +11,7 @@ public class HashMapFuckAround {
 	public static void main(String[] args) {
 		// populate the price list
 		Map<String, String> barPrice= new HashMap<String,String>(){{
-			//TODO add the rest of the bars
+			
 			put("Oh Henry", "0.65");
 			put("Coffee Crisp", "0.80");
 			put("Aero", "0.60");
@@ -24,20 +24,35 @@ public class HashMapFuckAround {
 	// Loop over the entries 
 		for (Map.Entry<String, String>entry : barPrice.entrySet()) {
 			// Take the names from barPrice, get amount from Console
-			System.out.print("How many boxes of " + entry.getKey() + " ");
+			System.out.printf("%-75s", "How many boxes of " + entry.getKey() + " would you like (48 bars per box) ? ");
 			barNum.put(entry.getKey(), new Scanner(System.in).next());
 			}
 		
 	//loop again
 		float subTotal = 0;
+		float HST;
+		float amtDue;
+		float HSTrate = 0.13f;
 		for(Map.Entry<String, String>entry : barPrice.entrySet()) {
 			String barName = entry.getKey();
 			String barAmt = barNum.get(barName);
 			float barTotal = (Float.parseFloat(barPrice.get(barName)) * (Float.parseFloat(barAmt)) * 48);
-			System.out.print(barAmt + " boxes of " + barName + " ($" + barPrice.get(barName) + " x 48) = $(" );
-			System.out.printf("%.2f)\n",barTotal);
+			System.out.printf("%-25s", barAmt + " boxes of " + barName);
+			System.out.printf("%19s", "($" + barPrice.get(barName) + " x 48) = $" );
+			System.out.printf("%2.2f\n",barTotal);
 			subTotal += barTotal;
-			System.out.print(subTotal);
 		}
+		
+		HST = subTotal * HSTrate;
+		amtDue = subTotal + HST;
+		
+		System.out.printf("\n--------------------------------------------------\n");
+		System.out.printf("Sub Total                               = $%3.2f \n", subTotal);
+		System.out.printf("HST                                     =  $%03.2f \n", HST);
+		System.out.printf("==================================================\n");
+		System.out.printf("Amount Due                              = $%3.2f", amtDue);
+		
+			
+			
 	}
 }
