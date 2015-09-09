a6168064cd381b906fb8d1f82344e0230daabd4a
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-09 17:19:29 -0400

    Cleaned code, changed order of user queries to match assignment

--- BallPackerProgram.java
+++ BallPackerProgram.java
@@ -8,19 +8,19 @@ public class BallPackerProgram{
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
-		double height = (int) GetDim("Box Height");
-		double width = (int) GetDim("Box Width");
-		double length = (int) GetDim("Box length");
-		double ballLen = (GetDim("Ball Radius")) * 2;
-		double ballNum = (Math.floor(height/ballLen) * Math.floor(width/ballLen) * Math.floor(length/ballLen));
 		
-			
+	// Grab the dimensions from the user
+		double length = GetDim("Box length");		
+		double width =  GetDim("Box Width");
+		double height = GetDim("Box Height");		
+		double ballLen = GetDim("Ball Radius") * 2;
+	// Find how many whole balls fit into each dimension, multiple to get total number of balls
+		double ballNum = (Math.floor(height/ballLen) * Math.floor(width/ballLen) * Math.floor(length/ballLen));
 		JOptionPane.showMessageDialog(null, "The Box can hold " + (int) Math.floor(ballNum) + " Balls");
 	}
-	public static float GetDim(String DimName) {
-		float BoxDim;
-		BoxDim = Float.parseFloat(JOptionPane.showInputDialog( "What is the " + DimName));
-		return BoxDim;
+	public static double GetDim(String DimName) {
+		double boxDim;
+		boxDim = Double.parseDouble(JOptionPane.showInputDialog( "What is the " + DimName));
+		return boxDim;
 		}
 }
