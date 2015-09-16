71e05d07b001ba9307dd97a1940dbd08be90d28f
Peter Kowalchuk-Reid <peter.kowalchuk.reid@gmail.com>
2015-09-16 13:14:41 -0400

    Changed to match assignment

--- BoxWrapperProgram.java
+++ BoxWrapperProgram.java
@@ -9,13 +9,13 @@ public class BoxWrapperProgram {
 	 */
 	public static void main(String[] args) {
 		
-		int boxWidth = GetDim("Width");
 		int boxLen = GetDim("Length");
+		int boxWidth = GetDim("Width");
 		int boxHeight = GetDim("Height");
 		
 		int papLen = ((boxHeight * 2) + boxLen) + 1;
 		int papWidth = ((boxWidth * 2) + (boxHeight * 2) + 1);
-		JOptionPane.showMessageDialog(null, papWidth + " * " + papLen + " cm", null, 0);
+		JOptionPane.showMessageDialog(null, papWidth + " cm" + " * " + papLen + " cm", null, 0);
 	}
 	public static int GetDim(String DimName) {
 	// Takes what dimension you want, asks the user for it, returns the dimension
